43b0ac60def03a690e6c2fe9631a12b3f8695983
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-05-06 10:41:47 -0400

    completed button template functionality. a good response increments the score by one and a bad response decrements by one.

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -2,30 +2,43 @@ import './Card.css'
 import { useState } from 'react';
 import useAuth from "../../hooks/useAuth";
 import axios from "axios";
+import { useEffect } from 'react';
 
 const DeckCard = (props) => {
  const [showDef,setShowDef] = useState("hide")
- const [score,setScore] = useState(0);
+ const [score,setScore] = useState();
  const [user, token] = useAuth();
- let word = 'river'
+ const [word,setWord] = useState();
+
+
+ useEffect(() => {
+        getWord();
+        // console.log(word.score)
+    }, [])
 
 
  function handleDefClick(){
      setShowDef("show")
  }
 
- function handleScoreClick(){
-     updateWordScore();
- }
+function handleGoodClick(){
+  setScore(word.score + 1)
+  updateWordScore()
+}
+function handleBadClick(){
+  setScore(word.score -1)
+  updateWordScore()
+}
+
 
 
  const updateWordScore = async () => {
    let body = {
-     score: 10
+     score: score
    };
     try {
       let response = await axios.patch(
-        `http://127.0.0.1:8000/api/decks/update_word/${word}/`,
+        `http://127.0.0.1:8000/api/decks/update_word/${word.word}/`,
         body,
         {
         headers: {
@@ -36,6 +49,24 @@ const DeckCard = (props) => {
       console.log(error.message)
     }
   }
+
+  const getWord = async () =>{
+    try {
+      let response = await axios.get(
+        `http://127.0.0.1:8000/api/decks/get_word/${props.parentDeck[0].words[0].word}/`,
+        {
+          headers: {
+            Authorization: 'Bearer ' + token,
+          },
+        }
+      );
+      setWord(response.data);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+
+
   
 
     return (  
@@ -47,7 +78,13 @@ const DeckCard = (props) => {
             </div>
             <p className={showDef}> {props.parentDeck[0].words[0].definition}</p>
             <div> 
-             <button onClick = {handleScoreClick} > Score </button>  
+             <button onClick = {handleGoodClick} > Good </button>  
+            </div>
+            <div> 
+             <button onClick = {handleBadClick} > Bad </button>  
+            </div>
+            <div> 
+             <button> Neutral </button>  
             </div>
         </div>
     );

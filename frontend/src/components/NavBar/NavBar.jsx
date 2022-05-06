5c73278b8e75de10f26ddaf81597f2babbe55e43
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-05-06 15:20:59 -0400

    added a modal component that will hold the search bar tied to the api call.

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -16,8 +16,11 @@ const Navbar = () => {
   const navigate = useNavigate();
   const[query,setQuery] = useState('')
   const[word,setWord] = useState('')
-  console.log(query)
-  
+  // console.log(query)
+  // console.log(word[0].shortdef[0])
+
+
+
   const handleSearch = () =>{
     getWebsterWord();
   }
@@ -27,7 +30,7 @@ const Navbar = () => {
         `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${query}?key=939c6c16-c538-4b7c-8ee9-f01f9725a621`
       );
       setWord(response.data);
-      console.log(`WEBSTER RESPONSE: ${word}`)
+      console.log("alert, api call made")
     } catch (e) {
       console.log(e.message);
     }
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import axios from "axios";
 import Card from "../../components/Card/Card";
+import MainModal from "../../components/Modal/Modal";
 
 const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
@@ -38,6 +39,7 @@ const HomePage = () => {
   return (
     <div className="container">
       <Card parentDecks = {decks}/>
+      <MainModal/>
     </div>
   );}
 };

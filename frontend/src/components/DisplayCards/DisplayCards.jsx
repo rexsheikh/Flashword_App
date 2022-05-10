ffeee09ddc255227b622b955161b7ff5504cd2a8
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-05-10 16:56:38 -0400

    wrote a function to transform the array of objects from the parentDecks to later place them in the proper format for the google react chart.

--- frontend/src/components/DisplayCards/DisplayCards.jsx
+++ frontend/src/components/DisplayCards/DisplayCards.jsx
@@ -5,10 +5,11 @@ import { Container,Row,Col } from "react-bootstrap";
 
 
 const DisplayCards = (props) => {
-  
 
-
-
+    console.log(props.parentDecks)
+    const words = props.parentDecks.map((deck)=> deck.words.map((date) => date.dates.map((date)=>date.date)))
+    console.log(words)
+    
     return ( 
         <Container style = {{height:"400px"}}>
           <Row>
--- frontend/src/components/TitleCard/TitleCard.jsx
+++ frontend/src/components/TitleCard/TitleCard.jsx
@@ -11,6 +11,31 @@ const TitleCard = (props) => {
         setShowTitle(-1)
     }
 
+    let data = [
+        [
+          {
+            type: "date",
+            id: "Date",
+          },
+          {
+            type: "number",
+            id: "Won/Loss",
+          },
+        ],
+        [new Date(2013, 0, 4), 38177],
+        [new Date(2013, 0, 5), 38705],
+        [new Date(2013, 0, 12), 38210],
+        [new Date(2013, 0, 13), 38029],
+        [new Date(2013, 0, 19), 38823],
+        [new Date(2013, 0, 23), 38345],
+        [new Date(2013, 0, 24), 38436],
+        [new Date(2013, 0, 30), 38447],
+      ];
+
+    
+
+    
+
     return ( 
         <div>
             <Card style = {{position:"absolute", zIndex:`${showTitle}`,width:"300px",height:"300px",border:"nond",width: '18rem', backgroundColor:"#EFEAD8", borderRadius:"20px", fontFamily: "Roboto Mono, monospace"}}>
@@ -18,10 +43,10 @@ const TitleCard = (props) => {
                 <button onClick = {handleClick}> Click to Start </button>
                 <Card.Body>
                     <Chart stye = {{zIndex:"100", position:"absolute"}}
-                    chartType="ScatterChart"
-                    data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+                    chartType="Calendar"
                     width="100%"
                     height="400px"
+                    data = {data}
                     />
                 </Card.Body>
             </Card>

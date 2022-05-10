5c76ccdfa91f701155e32abce43a9f55555842e4
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-05-10 12:37:46 -0400

    Added some more styling to the deckCard component.

--- frontend/src/components/DeckCard/DeckCard.jsx
+++ frontend/src/components/DeckCard/DeckCard.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import useAuth from "../../hooks/useAuth";
 import axios from "axios";
 import { useEffect } from 'react';
-import { Card,} from 'react-bootstrap';
+import { Card} from 'react-bootstrap';
 import { Badge } from 'react-bootstrap';
 
 const DeckCard = (props) => {
@@ -23,16 +23,32 @@ const DeckCard = (props) => {
 
 function handleGoodClick(currentWord,currentWordScore){
   getWord(currentWord)
-  setScore(currentWordScore)
+  setScore(currentWordScore + 1)
+  updateWordScore(currentWord,currentWordScore)
+  setShowDef("hide")
+  setIndex(index + 1)
+  
+}
+function handleEasyClick(currentWord,currentWordScore){
+  getWord(currentWord)
+  setScore(currentWordScore + 2)
   updateWordScore(currentWord,currentWordScore)
   setShowDef("hide")
   setIndex(index + 1)
   
 }
 
-function handleBadClick(currentWord,currentWordScore){
+function handleHardClick(currentWord,currentWordScore){
   getWord(currentWord)
-  setScore(currentWordScore)
+  setScore(currentWordScore -1)
+  updateWordScore(currentWord,currentWordScore)
+  setShowDef("hide")
+  setIndex(index + 1)
+
+}
+function handleAgainClick(currentWord,currentWordScore){
+  getWord(currentWord)
+  setScore(currentWordScore -2)
   updateWordScore(currentWord,currentWordScore)
   setShowDef("hide")
   setIndex(index + 1)
@@ -74,27 +90,28 @@ function handleBadClick(currentWord,currentWordScore){
   }
 
   return (  
-      <div className="row">
-          <Card variant = "main" style={{ width: '18rem', backgroundColor:"#EFEAD8", borderRadius:"20px", fontFamily: "Roboto Mono, monospace"}}>
-              <Card.Body>
-                <Card.Title>{props.deck.words[index].word}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">
-                    <button className = "card-button" onClick = {handleDefClick}> Show Definition</button>
-                </Card.Subtitle>
-                <Card.Text>
-                  <p className = {showDef}> {props.deck.words[index].definition} </p>
-                </Card.Text>
-                  {props.deckId}
-                <Card.Text>
-                </Card.Text>
-                <Card.Footer>
-                      <button className='card-button' onClick = {() => handleGoodClick(props.deck.words[index].word, props.deck.words[index].score)} > Good </button>  
-                      <button className='card-button'> Neutral </button>  
-                      <button className='card-button' onClick = {() => handleBadClick(props.deck.words[index].word, props.deck.words[index].score)} > Bad </button> 
-                </Card.Footer>
-              </Card.Body>
-            </Card>
-          </div>
+  <div className="title-example">
+    <Card style={{ width: '18rem', backgroundColor:"#EFEAD8", borderRadius:"20px", fontFamily: "Roboto Mono, monospace"}}>
+        <Card.Body>
+          <Card.Title>{props.deck.words[index].word}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+              <button className = "card-button" onClick = {handleDefClick}> Show Definition</button>
+          </Card.Subtitle>
+          <Card.Text>
+            <p className = {showDef}> {props.deck.words[index].definition} </p>
+          </Card.Text>
+            {props.deckId}
+          <Card.Text>
+          </Card.Text>
+          <Card.Footer>
+            <button className='card-button' onClick = {() => handleAgainClick(props.deck.words[index].word, props.deck.words[index].score)} > Again </button>  
+            <button className='card-button' onClick = {() => handleHardClick(props.deck.words[index].word, props.deck.words[index].score)} > Hard </button>  
+            <button className='card-button' onClick = {() => handleGoodClick(props.deck.words[index].word, props.deck.words[index].score)} > Good </button>   
+            <button className='card-button' onClick = {() => handleEasyClick(props.deck.words[index].word, props.deck.words[index].score)} > Easy </button> 
+          </Card.Footer>
+        </Card.Body>
+      </Card>
+  </div>
         
     );
 }

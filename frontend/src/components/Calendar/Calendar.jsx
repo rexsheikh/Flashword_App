7560e132b69944298822494fa7c18c12f682c035
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-05-11 22:41:06 -0400

    styled the homepage.

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -1,19 +1,8 @@
-// const words = props.parentDecks.map((deck)=> deck.words.map((date) => date.dates.map((date)=>date.date)))
-// const flat = words.flat(Infinity)
-// function getDateArray(arr = flat){
-//     let res = [];
-//     for(let i = 0; i < arr.length; i++){
-//         res.push([new Date(arr[i])])
-//     }
-//     return res
-// }
-
 import { useEffect } from 'react';
 import { Chart } from 'react-google-charts';
 import { useState } from 'react'
 import axios from 'axios';
 import useAuth from '../../hooks/useAuth';
-
 
 const Calendar = (props) => {
 
@@ -53,19 +42,7 @@ function scoreAgg(arr){
     return dict  
 }
 
-// console.log(dates)
-// let sampleDate = dates[4].date
-// let new_date = sampleDate.split('-')
-// function ints(arr){
-//     let res = []
-//    for(let i = 0; i < new_date.length; i++){
-//     res.push(parseInt(new_date[i]))
-// }
-// return res
-// }
-// let int_list = ints(new_date)
-// console.log(int_list)
-// let res = new Date (int_list[0],int_list[1],int_list[2])
+
 
 function formatDates(str){
 let res = []
@@ -99,22 +76,19 @@ function packageData(arr){
     return container
 }
 
-let test = packageData(dates)
-console.log(test)
 
-let data = test
-
+// <Chart
+// chartType="Calendar"
+// width="100%"
+// height="400px"
+// data= {data}
+// />
 
 
  
     return (  
     <div>
-        <Chart
-        chartType="Calendar"
-        width="100%"
-        height="400px"
-        data= {data}
-        />
+ 
     </div>
     );
 
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -5,34 +5,23 @@ import AuthContext from "../../context/AuthContext";
 import axios from "axios";
 import "./NavBar.css";
 
-
-// Key (Dictionary):
-// 939c6c16-c538-4b7c-8ee9-f01f9725a621
-// Key (Thesaurus):
-// ea9d4b59-071f-4c46-b692-47b3f4b4c127
-
 const Navbar = () => {
   const { logoutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
-
-  // console.log(query)
-  // console.log(word[0].shortdef[0])
-
-
-
-
 
   return (
     <div className="navBar">
       <ul>
         <li className="brand">
           <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>Flashword</b>
+            <b style = {{fontFamily: "Roboto Mono, monospace",fontSize:"50px"}}>Fl</b>
+            <img style = {{marginBottom:"35px"}} src = 'https://upload.wikimedia.org/wikipedia/commons/0/0a/Latin_lowercase_alpha.svg'></img>
+            <b style = {{fontFamily: "Roboto Mono, monospace",fontSize:"50px"}}>shWord</b>
           </Link>
         </li>
         <li>
           {user ? (
-            <button onClick={logoutUser}>Logout</button>
+            <button style = {{backgroundColor:"#6D8B74", borderRadius:"20px", border:"10px", fontFamily: "Roboto Mono, monospace", marginTop:"17px"}} onClick={logoutUser}>Logout</button>
           ) : (
             <button onClick={() => navigate("/login")}>Login</button>
           )}

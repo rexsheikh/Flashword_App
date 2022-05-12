afa753df26baa81f542c59801403422c9c0b4923
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-05-11 20:35:27 -0400

    completed the google react chart to update dynamically.

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -53,51 +53,56 @@ function scoreAgg(arr){
     return dict  
 }
 
-let test = scoreAgg(dates)
-console.log(test)
-
-
-//  console.log(dates)
-//  let sampleDate = dates[4].date
-//  let new_date = sampleDate.split('-')
-//  function ints(arr){
-//      let res = []
-//     for(let i = 0; i < new_date.length; i++){
-//      res.push(parseInt(new_date[i]))
-//  }
-//  return res
+// console.log(dates)
+// let sampleDate = dates[4].date
+// let new_date = sampleDate.split('-')
+// function ints(arr){
+//     let res = []
+//    for(let i = 0; i < new_date.length; i++){
+//     res.push(parseInt(new_date[i]))
+// }
+// return res
 // }
 // let int_list = ints(new_date)
 // console.log(int_list)
 // let res = new Date (int_list[0],int_list[1],int_list[2])
 
-console.log(dates)
+function formatDates(str){
+let res = []
+let lst = str.split('-')
+for(let i = 0; i < lst.length; i++){
+    res.push(parseInt(lst[i]))
+}
+return new Date (res[0],res[1],res[2])
+}
 
+function packageData(arr){
+    let container = []
+    let headers =     [
+        {
+          type: "date",
+          id: "Date",
+        },
+        {
+          type: "number",
+          id: "reviews",
+        },
+      ]
+      container.push(headers)
+    let score_dict = scoreAgg(arr)
+    console.log(`score dict: ${score_dict}`)
+    let entries = Object.entries(score_dict)
+    console.log(`entries:${entries}`)
+    for(let i = 0; i < entries.length; i++){
+        container.push([formatDates(entries[i][0]),entries[i][1]])
+    }
+    return container
+}
 
+let test = packageData(dates)
+console.log(test)
 
-
-let data = [
-    [
-      {
-        type: "date",
-        id: "Date",
-      },
-      {
-        type: "number",
-        id: "Won/Loss",
-      },
-    ],
-    [new Date(2022, 2, 4), 10],
-    [new Date(2022, 2, 5), 11],
-    [new Date(2022, 2, 12),0],
-    [new Date(2022, 2, 13),9],
-    [new Date(2022, 2, 19),3],
-    [new Date(2022, 2, 23),5],
-    [new Date(2022, 2, 24),6],
-    [new Date(2022, 2, 30),7],
-    [new Date(2022,3,10),5],
-  ];
-
+let data = test
 
 
 
